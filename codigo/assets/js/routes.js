3ab6a960d35f7f48c0a7bab81db777a67e075e55
João Mateus Gomes Sales Barbosa <joaomgsb@gmail.com>
2023-11-08 21:26:54 -0300

    newsletter

---
+++ codigo/assets/js/routes.js
@@ -0,0 +1,4 @@
+export const routes = {
+    videos: "https://jsonserver.joaomgsb.repl.co/videos",
+    highlights: "https://jsonserver.joaomgsb.repl.co/highlights"
+}
---
+++ codigo/assets/js/script.js
@@ -0,0 +1,46 @@
+import { carouselVideos, Highlights } from './component/videos'
+import { routes } from './routes.js'
+import { loading } from './loading.js'
+
+const divCarouselVideos = document.getElementById('carousel-videos')
+const divHighlights = document.getElementById('highlights')
+
+loading(true)
+const requestVideos = await fetch(routes.videos)
+const jsonVideos = await requestVideos.json()
+
+divCarouselVideos.innerHTML = await carouselVideos(jsonVideos)
+
+const requestHighlights = await fetch(routes.highlights)
+const jsonHighlights = await requestHighlights.json()
+
+divHighlights.innerHTML = await Highlights(jsonHighlights)
+
+
+
+const owl = $('.owl-carousel').owlCarousel({
+    center:true,
+    loop: true,
+    arrows: true,
+    dots: true,
+    margin: 10,
+    items:4,
+    nav:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        700:{
+            items:2,
+            nav:true
+        },
+        1000:{
+            items:4,
+            nav:true,
+        }
+    }
+})
+
+loading(false, 4000)
