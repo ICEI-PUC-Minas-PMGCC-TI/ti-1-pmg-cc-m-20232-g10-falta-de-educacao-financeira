2b3b033d2d9c3a6cab7ee04f7f65ed6bac05167e
Bernardo Ladeira Kartabil <beladeira1.000@gmail.com>
2023-11-14 17:13:50 -0300

    Tentativa de fazer o gráfico, não deu certo

--- codigo/assets/js/component/grafico-linhas-desp.js
+++ codigo/assets/js/component/grafico-linhas-desp.js
@@ -1,105 +1,63 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('https://jsonserver-proknow.joopaulopaulo33.repl.co/usuarios')
-        .then(response => response.json())
-        .then(data => {
-            const relevantUsers = data.filter(entry => entry.incomings || entry.expenses);
-            const monthLabels = Array.from({ length: 12 }, (_, i) => {
-                const date = new Date(2023, i, 1);
-                return date.toLocaleString('default', { month: 'long' });
-            });
+   // Função para obter um usuário específico pelo ID
+   async function fetchUserById(userId) {
+    debugger
+    const apiUrl = "https://jsonserver-proknow.joopaulopaulo33.repl.co/usuarios";
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const user = data.find(u => u.id === userId);
+    return user;
+}
 
-            const ctx = document.getElementById('myChart').getContext('2d');
+// Função para criar e renderizar o gráfico
+async function createChart(userId) {
+    const user = await fetchUserById(userId);
 
-            relevantUsers.forEach(user => {
-                if (user.id === 27) {
-                    if (user.incomings) {
-                        if (user.incomings && user.expenses) {
-                            const salario = user.incomings.salario ? parseFloat(user.incomings.salario.replace(',', '')) : 0;
-                            const bonus = user.incomings.bonus ? parseFloat(user.incomings.bonus.replace(',', '')) : 0;
-                            const ativos = user.incomings.hasInvestments && user.incomings.ativos ? parseFloat(user.incomings.ativos.replace(',', '')) : 0;
-                        
-                            const receitas = salario + bonus + ativos;
+    if (!user) {
+        console.error("Usuário não encontrado.");
+        return;
+    }
 
-                        
-                    
-                        }
-                        
+    // Organizar os dados para o gráfico
+    const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const receitasData = user.incomings ? parseFloat(user.incomings.salario.replace(",", "")) + parseFloat(user.incomings.bonus.replace(",", "")) : 0;
 
-                        else if (user.expenses) {
-                            const despesasFixas = user.expenses.despesas ? parseFloat(user.expenses.despesas.replace('R$', '').replace(',', '')) : 0;
-                            const despesasMes = user.expenses.despesasMes ? parseFloat(user.expenses.despesasMes.replace('R$', '').replace(',', '')) : 0;
-                            const despesasLazer = user.expenses.lazer ? parseFloat(user.expenses.lazer.replace('R$', '').replace(',', '')) : 0;
-                        
-                            const despesas = despesasFixas + despesasMes + despesasLazer;
-                        
-                            const userChart = document.createElement('canvas');
-                            userChart.id = `chart_${user.id}`;
-                            document.body.appendChild(userChart);
-                        
-                            new Chart(userChart, {
-                                type: 'line',
-                                data: {
-                                    labels: ['Despesas Fixas', 'Despesas Mensais', 'Despesas com Lazer'],
-                                    datasets: [{
-                                        label: `Gráfico de Despesas de ${user.nome}`,
-                                        data: [despesasFixas, despesasMes, despesasLazer],
-                                        borderColor: [
-                                            'rgba(255, 99, 132, 1)',
-                                            'rgba(255, 159, 64, 1)',
-                                            'rgba(153, 102, 255, 1)'
-                                        ],
-                                        borderWidth: 1,
-                                        fill: false
-                                    }]
-                                },
-                                options: {
-                                    responsive: true,
-                                    maintainAspectRatio: false,
-                                    scales: {
-                                        y: {
-                                            beginAtZero: true
-                                        }
-                                    }
-                                }
-                            });
-                        }
-                    }
-                }
-            });
+    // Verificar se o usuário possui despesas
+    const despesasData = user.expenses ? parseFloat(user.expenses.despesas.replace(",", "")) : 0;
 
-            const myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: monthLabels,
-                    datasets: [
-                        {
-                            label: 'Receitas',
-                            data: Array(12).fill(0),
-                            borderColor: 'rgba(75, 192, 192, 1)',
-                            borderWidth: 1,
-                            fill: false
-                        },
-                        {
-                            label: 'Despesas',
-                            data: Array(12).fill(0),
-                            borderColor: 'rgba(255, 99, 132, 1)',
-                            borderWidth: 1,
-                            fill: false
-                        }
-                    ]
+    // Criar o gráfico
+    const ctx = document.getElementById('myLineChart').getContext('2d');
+    const myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Receitas',
+                    data: Array(12).fill(receitasData),
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 2,
+                    fill: false
                 },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
+                {
+                    label: 'Despesas',
+                    data: Array(12).fill(despesasData),
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 2,
+                    fill: false
                 }
-            });
-        })
-        .catch(error => {
-            console.error('Erro ao buscar dados da API:', error);
-        });
-});
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    max: 5000,
+                    stepSize: 500
+                }
+            }
+        }
+    });
+}
+
+// Chamar a função para criar o gráfico com o ID do usuário desejado (substituir pelo ID real)
+createChart(24); // Exemplo: ID 24
