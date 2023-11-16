949a11780bce5b0da49b4a715248ac481ef6f0c8
Victor Souza Lima <you@example.com>
2023-11-16 17:50:43 -0300

    pre-commit

--- codigo/assets/js/cadastro_ganhos.js
+++ codigo/assets/js/cadastro_ganhos.js
@@ -1,10 +1,17 @@
 const apiPath = 'https://jsonserver-proknow.joopaulopaulo33.repl.co/usuarios';
 
 
+<<<<<<< HEAD
 async function readUsers() {
 
   await fetch(apiPath)
     .then(response => response.json())
+=======
+async function readIncoming() {
+
+  await fetch(apiPath)
+    .then(response => response.text())
+>>>>>>> salvar-no-db
     .then(users => {
       console.log(users);
     })
@@ -12,18 +19,28 @@ async function readUsers() {
       console.error('Erro ao ler usuarios', error);
       displayMessage("Deu ruim ae paizão");
     })
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> salvar-no-db
 }
 
 
 
+<<<<<<< HEAD
 function registerIncoming(obj) {
+=======
+function registerIncoming(user) {
+  debugger;
+>>>>>>> salvar-no-db
 
   fetch(apiPath, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
+<<<<<<< HEAD
     body: JSON.stringify(jsonBody + obj)
   })
 
@@ -34,15 +51,43 @@ function registerIncoming(obj) {
     .then(data => {
       console.log('Success:');
       console.log(data);
+=======
+    body: JSON.stringify(user)
+  })
+
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+>>>>>>> salvar-no-db
     })
     .catch(error => {
       console.error('Erro ao inserir contato via API', error);
 
     })
+<<<<<<< HEAD
 }
 
 
 
+=======
+
+  }
+
+  function updateIncoming(id, user) {
+    fetch(`${apiPath}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success: ', data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      })
+  }
+>>>>>>> salvar-no-db
 
 
 
